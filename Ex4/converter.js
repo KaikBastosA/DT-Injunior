9cdf89091af9c18a52fed83342af0a68bfa09237
Kaik Bastos <kaikbastos@id.uff.br>
2025-01-16 19:23:48 -0300

    feat(Ex4): Adicionado a solução para o exercício 4

---
+++ Ex4/converter.js
@@ -0,0 +1,10 @@
+function convert(currency) {
+    const euro = 6.10;
+    const dollar = 5.70;
+    let convertedCurrency = currency / euro;
+    let convertedCurrency2 = currency / dollar;
+    console.log(`O valor convertido em euro é: ${convertedCurrency.toFixed(2)} e em dólar é: ${convertedCurrency2.toFixed(2)}`);
+}
+
+let currency = parseFloat(prompt("Digite o valor em reais para converter em euro/dólar: "));
+convert(currency);
